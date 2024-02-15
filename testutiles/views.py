e1aef7cb46719b4567570b7b4904080793f61cb8
Umesh <umeshnishad3001@gmail.com>
2024-02-15 18:03:17 +0530

    my first commit on git repostiore

---
+++ testutiles/views.py
@@ -0,0 +1,67 @@
+# I have created this file -> umesh
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def analyze(request):
+    #Get the text
+    djtext = request.POST.get('text', 'default')
+
+    # Check checkbox values
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+    charctercounter = request.POST.get('charctercounter', 'off')
+
+    #Check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+
+    elif(fullcaps=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze.html', params)
+
+    elif(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        return render(request, 'analyze.html', params)
+
+    elif (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char != "\r":
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+        # Analyze the text
+        
+    elif charctercounter == "on":
+        analyzed = ""
+        # charactercount= 0
+        for char in djtext:
+            if char != ' ':
+                analyzed = len(djtext)
+        params = {'purpose': 'Charcter counter ', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', params)
+    else:
+        return HttpResponse("Error")
